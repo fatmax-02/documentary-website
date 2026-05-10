6a0073f7f6945e52df21ca70e7b1f3e87565e46b
b74739578-star <b74739578@gmail.com>
2026-05-10 22:06:33 +0400

    Update index.js

--- project 1/server/index.js
+++ project 1/server/index.js
@@ -4,72 +4,87 @@ import cors from "cors";
 import userModel from "./models/Users.js";
 import commentModel from "./models/Comment.js";
 
-    const app = express(); 
-    app.use(express.json()); 
-    app.use(cors()); 
+const app = express(); 
+app.use(express.json()); 
+app.use(cors()); 
 
-    app.listen(4005, () => { 
-    console.log("connected with 4005"); 
-    }); 
+app.listen(4005, () => { 
+  console.log("connected with 4005"); 
+}); 
 
-    const constring = 
-    "mongodb+srv://admin:project123@cproject.qajjc.mongodb.net/Login?retryWrites=true&w=majority&appName=Cproject"; 
-    mongoose.connect(constring); 
+const constring = "mongodb+srv://admin:project123@cproject.qajjc.mongodb.net/Login?retryWrites=true&w=majority&appName=Cproject"; 
+mongoose.connect(constring); 
 
-    //for login database
-    app.post("/adduser", async (req, res) => { 
-        const user = new userModel({ 
-        username: req.body.username, 
-        useremail: req.body.useremail, 
-        password: req.body.password, 
-        }); 
-        await user.save(); 
-        res.send("Signed in Successfully"); 
-        });
+// CREATE - Add new user with validation
+app.post("/adduser", async (req, res) => { 
+  const { username, useremail, password, age } = req.body;
+  
+  // Validation
+  if (!username || username.length < 3) {
+    return res.status(400).send("Username must be at least 3 characters");
+  }
+  if (!useremail || !useremail.includes("@") || !useremail.includes(".")) {
+    return res.status(400).send("Invalid email format");
+  }
+  if (!password || password.length < 6) {
+    return res.status(400).send("Password must be at least 6 characters");
+  }
+  if (age && (age < 18 || age > 100)) {
+    return res.status(400).send("Age must be between 18 and 100");
+  }
+  
+  const user = new userModel({ 
+    username: username, 
+    useremail: useremail, 
+    password: password,
+    age: age || 0,
+    isActive: true,
+    createdAt: new Date()
+  }); 
+  await user.save(); 
+  res.send("Signed in Successfully"); 
+});
 
-    app.get("/getAllUsers", async (req, res) => { 
-        const user = await userModel.find(); 
-        const count = await userModel.countDocuments({}); 
-        res.send({ user, count }); 
-    });
-    
+// READ - Get all users
+app.get("/getAllUsers", async (req, res) => { 
+  const user = await userModel.find(); 
+  const count = await userModel.countDocuments({}); 
+  res.send({ user, count }); 
+});
 
-    //for comments database
-    app.post("/addcomment", async (req, res) => { 
-        const comment = new commentModel({ 
-        comment: req.body.comment, 
-        }); 
-        await comment.save(); 
-        res.send("Thank you for commenting!"); 
-    }); 
+// CREATE - Add new comment
+app.post("/addcomment", async (req, res) => { 
+  const comment = new commentModel({ 
+    comment: req.body.comment, 
+  }); 
+  await comment.save(); 
+  res.send("Thank you for commenting!"); 
+});
 
-    //deleting user
-    app.delete("/delete/:id", async (req, res) => { 
-        try { 
-          const id = req.params.id; 
-          await userModel.findOneAndDelete({ _id: id }); 
-          const count = await userModel.countDocuments({}); 
-          const msg = "Selected Document Deleted "; 
-          res.send({ msg, count }); 
-        } catch (err) { 
-          console.log(err); 
-        } 
-      }); 
+// DELETE - Delete user by ID
+app.delete("/delete/:id", async (req, res) => { 
+  try { 
+    const id = req.params.id; 
+    await userModel.findOneAndDelete({ _id: id }); 
+    const count = await userModel.countDocuments({}); 
+    const msg = "Selected Document Deleted "; 
+    res.send({ msg, count }); 
+  } catch (err) { 
+    console.log(err); 
+  } 
+});
 
-    //updating user
-    app.put("/updateuser/:id", async (req, res) => { 
-      const id = req.params.id; 
-     
-      try { 
-        const userupdate = await userModel.findOne({ _id: id }); 
-        userupdate.username = String(req.body.username); 
-        userupdate.useremail = String(req.body.useremail); 
-        userupdate.password = String(req.body.password); 
-        await userupdate.save(); 
-        //const msg = "Document Updated "; 
-        res.send({ msg: "Document Updated!" }); 
-      } 
-      catch (err) { 
-        res.send({ error: "Failed to update!" }); 
-      } 
-    }); 
+// UPDATE - Update user by ID
+app.put("/updateuser/:id", async (req, res) => { 
+  const id = req.params.id; 
+  try { 
+    const userupdate = await userModel.findOne({ _id: id }); 
+    userupdate.username = String(req.body.username); 
+    userupdate.useremail = String(req.body.useremail); 
+    userupdate.password = String(req.body.password); 
+    await userupdate.save(); 
+    res.send({ msg: "Document Updated!" }); 
+  } catch (err) { 
+    res.send({ error: "Failed to update!" }); 
+  } 
+});

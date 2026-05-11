f09eab23bb8e29c458f65c91bf12db279ce9257d
b74739578-star <b74739578@gmail.com>
2026-05-11 15:08:06 +0400

    Update Login.js

--- project 1/client/src/component/Login.js
+++ project 1/client/src/component/Login.js
@@ -5,21 +5,23 @@ const Login = () => {
     const [username, setUsername] = useState("");
     const [useremail, setUseremail] = useState("");
     const [password, setPassword] = useState("");
+    const [age, setAge] = useState("");
     const [resMsg, setresMsg] = useState("");
 
-    const newUser =() => {
+    const newUser = () => {
         axios
         .post("http://localhost:4005/adduser", { 
             username: username, 
             useremail: useremail, 
-            password: password, 
-          }) 
-          .then((res) => { 
+            password: password,
+            age: age
+        }) 
+        .then((res) => { 
             setresMsg(res.data); 
-          }) 
-          .catch((err) => { 
+        }) 
+        .catch((err) => { 
             console.log(err); 
-          });
+        });
     };
 
   return (
@@ -33,25 +35,26 @@ const Login = () => {
             Username :
             <input type="text" class="form-control input-field" required
                  onChange={(e) => setUsername(e.target.value)}></input>
-            
         </div>
         <div class="input-box">
             E-Mail :
             <input type="email" class="form-control input-field" required
                  onChange={(e) => setUseremail(e.target.value)}></input>
-            
         </div>
         <div class="input-box">
             Password :
             <input type="password" class="form-control input-field" required
                  onChange={(e) => setPassword(e.target.value)}></input>
-            <br></br>
         </div>
-        
+        <div class="input-box">
+            Age :
+            <input type="number" class="form-control input-field" required
+                 onChange={(e) => setAge(e.target.value)}></input>
+        </div>
       </form>
-      <div class=" input-box">
+      <div class="input-box">
             <center>
-            <button type="button" onClick={newUser}  class="input-submit">SIGN IN</button>
+            <button type="button" onClick={newUser} class="input-submit">SIGN IN</button>
             </center>
         </div>
         <div> <br/>

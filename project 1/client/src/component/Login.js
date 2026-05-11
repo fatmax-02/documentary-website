030db69eb3b0e4719f09a22a37b0a07d975fd4a7
b74739578-star <b74739578@gmail.com>
2026-05-11 16:30:01 +0400

    Update Login.js

--- project 1/client/src/component/Login.js
+++ project 1/client/src/component/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
 
     const newUser = () => {
         axios
-        .post('${process.env.REACT_APP_API_URL}/adduser', { 
+        .post(`${process.env.REACT_APP_API_URL}/adduser`, { 
             username: username, 
             useremail: useremail, 
             password: password,
@@ -26,54 +26,55 @@ const Login = () => {
         });
     };
 
-  return (
-    <div class="container">
-      <div class="box">
-      <form class="form-control">
-        <div class="header">
-            <p>Log In</p>
+    return (
+        <div class="container">
+            <div class="box">
+                <form class="form-control">
+                    <div class="header">
+                        <p>Log In</p>
+                    </div>
+                    <div class="input-box">
+                        Username :
+                        <input type="text" class="form-control input-field" required
+                            onChange={(e) => setUsername(e.target.value)}></input>
+                    </div>
+                    <div class="input-box">
+                        E-Mail :
+                        <input type="email" class="form-control input-field" required
+                            onChange={(e) => setUseremail(e.target.value)}></input>
+                    </div>
+                    <div class="input-box">
+                        Password :
+                        <input type="password" class="form-control input-field" required
+                            onChange={(e) => setPassword(e.target.value)}></input>
+                    </div>
+                    <div class="input-box">
+                        Age :
+                        <input type="number" class="form-control input-field" required
+                            onChange={(e) => setAge(e.target.value)}></input>
+                    </div>
+                    <div class="input-box" style={{ marginTop: '5px' }}>
+                        <label style={{ fontSize: '13px', display: 'flex', alignItems: 'center', gap: '8px', fontWeight: 'normal' }}>
+                            <input type="checkbox" onChange={(e) => setAgreeToTerms(e.target.checked)} /> 
+                            I agree to the Terms and Conditions
+                        </label>
+                    </div>
+                </form>
+                <div class="input-box">
+                    <center>
+                        <button type="button" onClick={newUser} class="input-submit">SIGN IN</button>
+                    </center>
+                </div>
+                <div> 
+                    <br/>
+                    <h5 class="logh3">
+                        {resMsg}
+                    </h5>
+                </div>
+            </div>
+            <div class="wrapper"></div>
         </div>
-        <div class="input-box">
-            Username :
-            <input type="text" class="form-control input-field" required
-                 onChange={(e) => setUsername(e.target.value)}></input>
-        </div>
-        <div class="input-box">
-            E-Mail :
-            <input type="email" class="form-control input-field" required
-                 onChange={(e) => setUseremail(e.target.value)}></input>
-        </div>
-        <div class="input-box">
-            Password :
-            <input type="password" class="form-control input-field" required
-                 onChange={(e) => setPassword(e.target.value)}></input>
-        </div>
-        <div class="input-box">
-            Age :
-            <input type="number" class="form-control input-field" required
-                 onChange={(e) => setAge(e.target.value)}></input>
-        </div>
-        <div class="input-box" style={{ marginTop: '5px' }}>
-            <label style={{ fontSize: '13px', display: 'flex', alignItems: 'center', gap: '8px', fontWeight: 'normal' }}>
-                <input type="checkbox" onChange={(e) => setAgreeToTerms(e.target.checked)} /> 
-                I agree to the Terms and Conditions
-            </label>
-        </div>
-      </form>
-      <div class="input-box">
-            <center>
-            <button type="button" onClick={newUser} class="input-submit">SIGN IN</button>
-            </center>
-        </div>
-        <div> <br/>
-          <h5 class="logh3">
-            {resMsg}
-          </h5>
-        </div>
-      </div>
-      <div class="wrapper"></div>
-    </div>
-  )
+    )
 }
 
 export default Login

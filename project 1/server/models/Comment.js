bc2f272ea357e88eba2aa508354c925c361233cb
b74739578-star <b74739578@gmail.com>
2026-05-13 23:12:27 +0400

    Delete project 1 directory

--- project 1/server/models/Comment.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"; 
-
-const commentSchema = mongoose.Schema({ 
-    comment: { type: String, required: true }, 
-}); 
-
-const commentModel = mongoose.model("Comment", commentSchema); 
-export default commentModel; 
